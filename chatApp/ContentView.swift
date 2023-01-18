78187eaf614e0839aab8305f98a4d315c204354f
djinodissingar <dissingardjino@icloud.com>
2023-01-18 14:40:22 +0100

    Initial Commit

---
+++ chatApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  chatApp
+//
+//  Created by Djino Dissingar on 18/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chatApp/chatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppApp.swift
+//  chatApp
+//
+//  Created by Djino Dissingar on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct chatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
