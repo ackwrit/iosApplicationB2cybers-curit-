85cfaaa156cf7bcff8e0802fccaff0b6535e206e
djinodissingar <dissingardjino@icloud.com>
2023-01-18 15:53:57 +0100

    nouveau git iOS

--- chatApp/ContentView.swift
+++ chatApp/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var mail : String = ""
+    @State var password : String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+         
+            //Logo
+          Image("fate")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+            //titre
+            Text("Mon application")
+                .font(.largeTitle)
+                .foregroundColor(Color.red)
+                .bold()
+         
+            
+            
+            TextField("Entrer votre adresse",text:$mail)
+                .font(.largeTitle)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            SecureField("Entrer votre mot de passe ",text:$password)
+                .font(.largeTitle)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Spacer()
+            Button {
+                //Se connecter
+            } label: {
+                Text("Connexion")
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .background {
+                        Color.purple
+                    }
+                   
+                   
+                    .clipShape(Capsule())
+            }
+            Button {
+                //Se connecter
+            } label: {
+                Text("Inscription")
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .background {
+                        Color.purple
+                    }
+                   
+                   
+                    .clipShape(Capsule())
+            }
+
+           
+            Spacer()
+           
         }
+        
+        
+        
         .padding()
+        .background {
+           Image("soul_eater")
+                .resizable()
+               .aspectRatio(contentMode: .fill)
+        }
+        .edgesIgnoringSafeArea(.all)
+       
+        
+       
+        
+        
+        
     }
 }
 
